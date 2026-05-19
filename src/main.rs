2bc5df6c0fc5f7ecc84e52cc7f914b8e6c578bf7
peaknot <53933770+peaknot@users.noreply.github.com>
2026-05-19 21:14:23 +0800

    removed slint

--- src/main.rs
+++ src/main.rs
@@ -2,38 +2,12 @@ use clap::Parser;
 
 use crate::{errors::AppError, handles::init_db, structs::{AppState, Cli}};
 
-slint::include_modules!();
-
 mod errors;
 mod handles;
 mod structs;
 
 fn main() {
-    if let Err(err) = run() {
-        eprintln!("{err}");
-        std::process::exit(1);
-    }
-    if let Err(err) = app_window() {
-        eprintln!("{err}");
-        std::process::exit(1);
-    }
     
 }
 
-fn run() -> Result<(), AppError> {
-    
-    let conn = init_db("./pharma_subs.db")?;
 
-    let app_state = AppState { db: conn};
-
-    let args = Cli::parse();
-
-    args.command.execute(app_state.db)
-    
-}
-
-fn app_window() -> Result<(), slint::PlatformError> {
-    let main_window = AppWindow::new()?;
-
-    main_window.run()
-}
